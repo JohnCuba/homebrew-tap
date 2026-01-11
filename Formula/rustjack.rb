f5df5476258fef262179fb8f9e031debea28a634
John Cuba <47921077+JohnCuba@users.noreply.github.com>
2026-01-11 16:23:42 +0300

    feat: rustjack

---
+++ Formula/rustjack.rb
@@ -0,0 +1,15 @@
+class MyTool < Formula
+  desc "Flex terminal gambling"
+  homepage "https://github.com/johncuba/rustjack"
+  url "https://github.com/johncuba/rustjack/releases/download/v0.1.0/rustjack-x86_64-apple-darwin.tar.gz"
+  sha256 ""
+  license "MIT"
+
+  def install
+    bin.install "rustjack"
+  end
+
+  test do
+    system "#{bin}/rustjack", "--version"
+  end
+end
