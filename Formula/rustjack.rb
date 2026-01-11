e70f0e57b9387674f778ee551d2d0bdede8e69de
John Cuba <47921077+JohnCuba@users.noreply.github.com>
2026-01-11 20:15:07 +0300

    Add SHA256 checksum for Rustjack formula

--- Formula/rustjack.rb
+++ Formula/rustjack.rb
@@ -2,6 +2,7 @@ class Rustjack < Formula
   desc "Flex terminal gambling"
   homepage "https://github.com/johncuba/rustjack"
   url "https://github.com/johncuba/rustjack/releases/download/v.0.2.1/rustjack-x86_64-apple-darwin.tar.gz"
+  sha256 "4278417cf844bfc56931202514824ea398e23fdd64cc2eb6eea82837b26a8ee6"
   license "MIT"
 
   def install
