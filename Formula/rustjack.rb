0bd2e4404bbfc4ca1a45c96b2ef2ef97ef2a6d92
John Cuba <47921077+JohnCuba@users.noreply.github.com>
2026-01-11 21:13:47 +0300

    Fix formatting in rustjack.rb file

--- Formula/rustjack.rb
+++ Formula/rustjack.rb
@@ -1,8 +1,8 @@
 class Rustjack < Formula
   desc "Flex terminal gambling"
   homepage "https://github.com/johncuba/rustjack"
-  version "0.3.0"
   url "https://github.com/johncuba/rustjack/releases/download/v0.3.0/rustjack-x86_64-apple-darwin.tar.gz"
+  version "0.3.0"
   sha256 "a045dbae47a2d47f21d550da2f041504cbe86d3343bd7e6b815a9d43a685ade9"
   license "MIT"
 
