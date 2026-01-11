7108405a87e6ddd0b53c37c6871cac3ba1946d87
John Cuba <47921077+JohnCuba@users.noreply.github.com>
2026-01-11 20:21:45 +0300

    Update Rustjack URL for macOS binary

--- Formula/rustjack.rb
+++ Formula/rustjack.rb
@@ -1,7 +1,7 @@
 class Rustjack < Formula
   desc "Flex terminal gambling"
   homepage "https://github.com/johncuba/rustjack"
-  url "https://github.com/johncuba/rustjack/releases/download/v0.2.2/rustjack-.tar.gz"
+  url "https://github.com/johncuba/rustjack/releases/download/v0.2.2/rustjack-x86_64-apple-darwin.tar.gz"
   sha256 "3c27774fbd27b4dc4bab085a7233737394833be2f771e77ca430c2ff17173dc2"
   license "MIT"
 
