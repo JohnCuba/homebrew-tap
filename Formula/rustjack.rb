038d9c6f38832278ff8e44765f47778b534d1556
John Cuba <47921077+JohnCuba@users.noreply.github.com>
2026-01-11 20:10:06 +0300

    Rename RustJack class to Rustjack

--- Formula/rustjack.rb
+++ Formula/rustjack.rb
@@ -1,4 +1,4 @@
-class RustJack < Formula
+class Rustjack < Formula
   desc "Flex terminal gambling"
   homepage "https://github.com/johncuba/rustjack"
   url "https://github.com/johncuba/rustjack/releases/download/v.0.2.1/rustjack-x86_64-apple-darwin.tar.gz"
