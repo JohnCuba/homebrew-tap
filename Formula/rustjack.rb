38d9437bba9d61ea80eb33090d70020ecaf7bb06
John Cuba <47921077+JohnCuba@users.noreply.github.com>
2026-01-11 20:07:11 +0300

    Remove empty SHA256 from RustJack formula
    Remove empty SHA256 checksum for RustJack formula.

--- Formula/rustjack.rb
+++ Formula/rustjack.rb
@@ -2,7 +2,6 @@ class RustJack < Formula
   desc "Flex terminal gambling"
   homepage "https://github.com/johncuba/rustjack"
   url "https://github.com/johncuba/rustjack/releases/download/v.0.2.1/rustjack-x86_64-apple-darwin.tar.gz"
-  sha256 ""
   license "MIT"
 
   def install
