b6c8009c20ffd4fe4f77b41fbca73ee75c7b4c82
John Cuba <47921077+JohnCuba@users.noreply.github.com>
2026-01-11 18:05:04 +0300

    rustjack v.0.2.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/rustjack.rb
+++ Formula/rustjack.rb
@@ -1,7 +1,7 @@
 class RustJack < Formula
   desc "Flex terminal gambling"
   homepage "https://github.com/johncuba/rustjack"
-  url "https://github.com/johncuba/rustjack/releases/download/v0.2.0/rustjack-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/johncuba/rustjack/releases/download/v.0.2.1/rustjack-x86_64-apple-darwin.tar.gz"
   sha256 ""
   license "MIT"
 
